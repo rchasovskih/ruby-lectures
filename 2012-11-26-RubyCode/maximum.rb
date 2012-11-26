c4e668ec89a26c5453e458689bdd03829a3f36e2
user <user@test.ru>
2012-11-26 18:21:56 +0400

    Add Ruby Script

---
+++ 2012-11-26-RubyCode/maximum.rb
@@ -0,0 +1,12 @@
+puts "Enter value for a: "
+a = gets
+puts "Enter value b: "
+b = gets
+
+if a > b
+  puts"Chislo #{a} bolshe #{b}"
+elsif a == b
+  puts "Chislo #{a} rovno #{b}"
+else
+  puts "Chislo #{a} menshe #{b}"
+end
